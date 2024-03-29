0bfbc930f0489c6d4889dfcffdba5673a90bf93b
DamianRondinon <95187328+DamianRondinon@users.noreply.github.com>
2024-03-29 20:58:38 -0300

    Modification of global CSS styles

--- project_landingPage/components/Navbar.jsx
+++ project_landingPage/components/Navbar.jsx
@@ -13,7 +13,10 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className="absolute w-[50%] inset-0" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img src="/logo-kawaii.svg" alt="logo" className="w-[5rem] h-[5rem] object-contain"/>
+    </div>
   </motion.nav>
 );
 
